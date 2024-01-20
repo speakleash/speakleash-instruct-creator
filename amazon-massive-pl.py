c044aae2f7805bd8928369fc9a6078f9b7b70a41
Paweł Kiszczak <pawel.kiszczak@icloud.com>
2024-01-20 18:36:16 +0100

    update amazon-massive-pl instruction generator

--- amazon-massive-pl.py
+++ amazon-massive-pl.py
@@ -195,8 +195,20 @@ main_dict = {
 }
 
 ### FUNCTIONS
-def download_and_unzip(url, 
-					   destination_folder='downloaded'):
+def download_and_unzip(url: str, 
+					   destination_folder: str ='downloaded'):
+	"""
+	Download a file from the given URL and unzip it if it is in tar.gz or tgz format.
+
+	Parameters:
+	- url (str): The URL of the file to be downloaded.
+	- destination_folder (str): The destination folder to save the downloaded and unpacked files.
+							   Defaults to 'downloaded' if not provided.
+
+	Returns:
+	- bool: True if the download and unpacking were successful, False otherwise.
+	"""
+	
 	# Create destination folder 
 	os.makedirs(destination_folder, exist_ok=True)
 
@@ -227,6 +239,26 @@ def download_and_unzip(url,
 		return False
 
 def load_instructions():
+	"""
+	Load instructions from a JSON Lines file, move the file to a specified directory,
+	and clean up the downloaded folder.
+
+	Returns:
+	- list: A list containing the loaded instructions as dictionaries.
+
+	Note:
+	The file path is set to 'downloaded/1.1/data/pl-PL.jsonl', and the loaded data is
+	moved to the 'data_dir' directory while maintaining its base name. The 'downloaded/'
+	directory is then removed.
+
+	Example:
+	```python
+	loaded_data = load_instructions()
+	print(f"Total instructions loaded: {len(loaded_data)}")
+	```
+
+	"""
+	# Prepare directories
 	fpath = os.path.abspath('downloaded/1.1/data/pl-PL.jsonl')
 	move_path = os.path.join(data_dir, os.path.basename(fpath))
 
@@ -235,31 +267,93 @@ def load_instructions():
 	
 	data = []
 
+	# Load in the data
 	with jsonlines.open(fpath) as f:
 		for line in f.iter():
 			data.append(line)	
 
+	# Delete unneseccasry files
 	shutil.move(fpath, move_path)
 	shutil.rmtree('downloaded/')
 
 	print(f"Loaded {len(data)} base instructions")
 	return data
 
-def get_categories(main_category, categories, k=4):
-	x = 0
-	while x != 5:
-		random_categories = random.choices(list(categories), k=5)
-		if main_category not in random_categories:
-			x = len(set(random_categories))
-	
-	random_categories.append(main_category)
+def get_categories_as_string(categories: List,
+							 main_category: str = None,
+							 k: int = 4):
+	"""
+	Generate a string representation of a list of random categories, including the specified main category.
+
+	Parameters:
+	- main_category (str): The main category that must be included in the generated list.
+	- categories (List): A list of available categories to choose from.
+	- k (int): The number of categories to generate (excluding the main category). Defaults to 4.
+
+	Returns:
+	- str: A string representation of the generated list of categories in a shuffled order.
+
+	Example:
+	```python
+	main_category = "Science"
+	all_categories = ["Technology", "Nature", "Math", "History", "Art"]
+	category_string = get_categories_as_string(main_category, all_categories, k=3)
+	print("Random Categories:", category_string)
+	```
+
+	Note:
+	If the provided list of categories has more than 10 elements, the function generates a shuffled
+	list of k random categories (excluding the main category) using random.choices. If there are 10
+	or fewer elements, the entire list is used. The main category is ensured to be included in the
+	final list, and the result is returned as a comma-separated string.
+	"""
+
+	if len(categories) > 10 and main_category: 
+		x = 0
+		while x != 5:
+			random_categories = random.choices(list(categories), k=5)
+			if main_category not in random_categories:
+				x = len(set(random_categories))
+		
+		random_categories.append(main_category)
+
+	else:
+		random_categories = categories
+
 	random.shuffle(random_categories)
-	
-	return random_categories
+	output = ", ".join(random_categories) + "."
+	return output
 
 def create_instructions(data: List[Dict],
-						main_dict: Dict,
-						):
+						main_dict: Dict):
+
+	"""
+	Create a list of instructions based on input data and a provided main dictionary.
+
+	Parameters:
+	- data (List[Dict]): A list of dictionaries containing input data, judgments, and other information.
+	- main_dict (Dict): A dictionary containing configuration details, categories, and instructions.
+
+	Returns:
+	- List[Dict]: A list of instructions as dictionaries.
+
+	Example:
+	```python
+	input_data = [...]  # List of dictionaries containing input data
+	main_configuration = {...}  # Dictionary containing configuration details
+
+	instructions = create_instructions(input_data, main_configuration)
+	```
+
+	Note:
+	The function creates instructions based on the given input data and main dictionary. It uses
+	configuration details, categories, and instructions to generate a list of dictionaries with
+	instructions, input data, output details, and other metadata. Two loops are used: the base loop
+	for 'intent' and 'scenario' types, and the inner loop for 'intent_score', 'grammar_score',
+	'spelling_score', and 'language_identification' types.
+	"""
+
+	# Setup variables
 	output = []
 	temp = {}
 
@@ -271,9 +365,12 @@ def create_instructions(data: List[Dict],
 		for item in data:
 			for worker in item['judgments']:
 
-				categories = get_categories(main_category=main_dict[type]['output'][item[type]], 
-											categories=main_dict[type]['categories'])
+				categories = get_categories_as_string(
+					main_category=main_dict[type]['output'][item[type]], 
+					categories=main_dict[type]['categories'])
+
 				'''
+				OPTIONAL DICTIONARY CREATION EXAMPLE
 				temp = {
 					'instruction': random.choice(main_dict[type]['instruction']),
 					'input': item['utt'],
@@ -284,13 +381,16 @@ def create_instructions(data: List[Dict],
 					'source_description': source_description,
 					'script_name': script_name
 				}
-
 				'''
-				temp['instruction'] = random.choice(main_dict[type]['instruction']) + str(categories)
+
+				temp['instruction'] = random.choice(main_dict[type]['instruction']) + categories
 				temp['input'] = item['utt']
 				temp['output'] = main_dict[type]['output'][item[type]]
+
+				# Categories not needed bue to them being included in the main instruction
 				#temp['categories'] = main_dict[type]['categories']
 				#temp['categories'] = categories
+
 				temp['source_name'] = source_name
 				temp['source_url'] = source_url
 				temp['source_description'] = source_description
@@ -309,10 +409,14 @@ def create_instructions(data: List[Dict],
 	for type in type_inner:
 		for item in data:
 			for worker in item['judgments']:
-				temp['instruction'] = random.choice(main_dict[type]['instruction'])
+
+				categories = get_categories_as_string( 
+					categories=main_dict[type]['categories'])
+
+				temp['instruction'] = random.choice(main_dict[type]['instruction']) + categories
 				temp['input'] = item['utt']
 				temp['output'] = main_dict[type]['output'][worker[type]]
-				temp['categories'] = main_dict[type]['categories']
+				#temp['categories'] = main_dict[type]['categories']
 				temp['source_name'] = source_name
 				temp['source_url'] = source_url
 				temp['source_description'] = source_description
@@ -327,6 +431,28 @@ def create_instructions(data: List[Dict],
 	return output
 
 def remove_duplicates(data, shuffle=False):
+	"""
+	Remove duplicate items from a list, optionally shuffling the unique items.
+
+	Parameters:
+	- data: The input list containing items to be deduplicated.
+	- shuffle (bool): If True, shuffle the unique items in the output list. Defaults to False.
+
+	Returns:
+	- list: A deduplicated list of items.
+
+	Example:
+	```python
+	input_data = [...]  # List containing items with potential duplicates
+	unique_data = remove_duplicates(input_data, shuffle=True)
+	```
+
+	Note:
+	The function removes duplicate items from the input list, maintaining the order of the first
+	occurrence of each item. The deduplicated list is returned, and if specified, it can be shuffled.
+	The function prints information about the deduplication process, including the number of
+	duplicates found and the size of the final output list.
+	"""
 	seen = set()
 	output = []
 
@@ -348,6 +474,26 @@ def remove_duplicates(data, shuffle=False):
 	return output
 
 def save_instructions(instructions, output_dir, json_file='amazon-massive-pl.json'):
+	"""
+	Save a list of instructions to a JSON file.
+
+	Parameters:
+	- instructions: The list of instructions to be saved.
+	- output_dir (str): The directory where the JSON file will be saved.
+	- json_file (str): The name of the JSON file. Defaults to 'amazon-massive-pl.json'.
+
+	Example:
+	```python
+	instruction_list = [...]  # List of instructions to be saved
+	save_instructions(instruction_list, '/path/to/output_directory')
+	```
+
+	Note:
+	The function saves the provided list of instructions to a JSON file located in the specified
+	output directory. The JSON file is formatted with an indentation of 4 spaces, and non-ASCII
+	characters are preserved. After successfully saving the instructions, a confirmation message
+	is printed, including the path to the saved file.
+	"""
 	fpath = os.path.join(output_dir, json_file)
 	with open(fpath, 'w', encoding='utf-8') as f:
 		json.dump(instructions, f, indent=4, ensure_ascii=False)
@@ -370,5 +516,5 @@ script_name = os.path.basename(__file__)
 download_and_unzip(url=source_url)
 data = load_instructions()
 instructions = create_instructions(data, main_dict)
-instructions_cleared = remove_duplicates(instructions)
+instructions_cleared = remove_duplicates(instructions, shuffle=True)
 save_instructions(instructions_cleared, output_dir)
