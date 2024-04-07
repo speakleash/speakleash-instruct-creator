947aa7cc391f431b5dc591bd8c8f2619fe45dc9f
Jan Sowa <jan.piotr.sowa@gmail.com>
2024-04-07 12:22:42 +0200

    First version of scripts to generate Polish orca math translation

---
+++ instructions_scripts/orca_math_create_english_docx.py
@@ -0,0 +1,75 @@
+from datasets import load_dataset
+import docx
+import json
+import os
+from utils.functions import get_dir_path
+
+# TODO: add info how to use this script
+
+dataset = load_dataset("microsoft/orca-math-word-problems-200k")
+questions, answers = dataset['train']['question'], dataset['train']['answer']
+base_dir = os.path.dirname(os.path.abspath(__file__))
+data_folder = get_dir_path("data") or os.path.join(base_dir, "data")
+translated_folder = os.path.join(data_folder, "translated")
+indexes_path = os.path.join(data_folder, 'docx_indexes.json')
+
+
+def split_dataset_into_indexes(question_arr, answer_arr, max_chars: int = 4800000) -> list:
+    """
+    Args:
+        question_arr: array with all questions
+        answer_arr: array with all answers
+        max_chars: maximum number of characters in single split
+
+    Returns: array indices between which the sum of all questions and answers (including margins)
+    is less than max_chars.
+    """
+    subset_indexes = []
+    current_chars_count = 0
+    start_index = 0
+    entry_margin = 12
+
+    for index, (question, answer) in enumerate(zip(question_arr, answer_arr)):
+        entry_length = len(question) + len(answer) + entry_margin
+
+        if current_chars_count + entry_length > max_chars:
+            subset_indexes.append((start_index, index - 1))
+            start_index = index
+            current_chars_count = 0
+
+        current_chars_count += entry_length
+
+    if current_chars_count > 0:
+        subset_indexes.append((start_index, len(question_arr)))
+
+    return subset_indexes
+
+
+indexes = split_dataset_into_indexes(questions, answers)
+
+
+def save_indexes(file_path: str) -> None:
+    with open(file_path, 'w') as file:
+        json.dump(indexes, file)
+
+
+os.makedirs(data_folder, exist_ok=True)
+os.makedirs(translated_folder, exist_ok=True)
+save_indexes(indexes_path)
+
+
+def generate_docx(questions, answers):
+    doc = docx.Document()
+    for row_idx in range(start, end):
+        doc.add_paragraph("#Q#\n")
+        doc.add_paragraph(questions[row_idx] + "\n")
+        doc.add_paragraph("#A#\n")
+        doc.add_paragraph(answers[row_idx] + "\n")
+    doc.save(os.path.join(data_folder, f"math_problems_to_translate_char_limit{doc_idx}.docx"))
+
+
+for doc_idx, (start, end) in enumerate(indexes):
+    generate_docx(questions, answers)
+
+print(".docx with questions and answers are created in " + data_folder)
+print("Now translate them and put into " + translated_folder)
---
+++ instructions_scripts/orca_math_create_json_from_docx.py
@@ -0,0 +1,112 @@
+import re
+import json
+import os
+
+from datasets import load_dataset
+from docx import Document
+
+from utils.functions import get_dir_path
+
+# TODO: add info how to use this script
+
+dataset = load_dataset("microsoft/orca-math-word-problems-200k")
+questions, answers = dataset['train']['question'], dataset['train']['answer']
+base_dir = os.path.dirname(os.path.abspath(__file__))
+data_folder = get_dir_path("data") or os.path.join(base_dir, "data")
+translated_folder = os.path.join(data_folder, "translated")
+indexes_path = os.path.join(data_folder, 'docx_indexes.json')
+
+
+def load_indexes(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+def remove_trailing_newline(text):
+    if text.endswith('\n'):
+        return text[:-1]
+    return text
+
+
+indexes = load_indexes(indexes_path)
+translated_json_path = os.path.join(translated_folder, 'json')
+
+
+paths = os.listdir(translated_folder)
+paths = [path for path in paths if path.endswith(".docx")]
+sorted_paths = sorted(paths, key=lambda x: int(re.search(r'\d+', x).group()))
+
+
+os.makedirs(translated_json_path, exist_ok=True)
+
+# for doc_idx in range(0, len(indexes)):
+for doc_idx in range(0, 1):
+    print("Document number: " + str(doc_idx))
+    # Load the DOCX document
+    doc_path = os.path.join(translated_folder, paths[doc_idx])
+    doc = Document(doc_path)
+
+    # Initialize variables
+    data = []
+    id_counter = 1
+
+    start_idx = indexes[doc_idx][0]
+    # Iterate through the paragraphs in the document
+    for i in range(0, len(doc.paragraphs), 4):
+        if doc.paragraphs[i].text == "#Q#\n" and doc.paragraphs[i + 2].text == "#A#\n":
+            # Create a dictionary for the question-answer set
+            entry = {
+                "instruct": remove_trailing_newline(doc.paragraphs[i + 1].text),
+                "input": "",
+                "output": remove_trailing_newline(doc.paragraphs[i + 3].text),
+                "instruct_eng": questions[start_idx + i // 4],
+                "output_eng": answers[start_idx + i // 4],
+                "source_name": "microsoft-orca_math_problems",
+                "source_url": "https://huggingface.co/datasets/microsoft/orca-math-word-problems-200k",
+                "source_description": "Pary pytanie-odpowiedź powstały na bazie zestawu danych orca math word problems, a następnie przetłumaczone maszynowo.",
+                "script_name": "translate_orca_math_problems.py",
+                "id": start_idx + i // 4,
+                "status": "",
+                "updated_by": ""
+            }
+            # Append the entry to the data list
+            data.append(entry)
+        else:
+            print("something went wrong for entry " + str(i // 4))
+
+
+
+    # Save the data to a JSONL file
+    jsonl_path = os.path.join(translated_json_path, f"math_problems_converted{doc_idx}.jsonl")
+    with open(jsonl_path, 'w', encoding='utf-8-sig') as outfile:
+        for entry in data:
+            json.dump(entry, outfile, ensure_ascii=False)
+            outfile.write('\n')
+
+
+paths = os.listdir(translated_json_path)
+pahts = sorted(paths, key=lambda x: int(re.search(r'\d+', x).group()))
+
+base_dir = os.path.dirname(os.path.abspath(__file__))
+output_path = get_dir_path("output") or os.path.join(base_dir, "output")
+with open(os.path.join(output_path, 'polish_orca_math.json'), 'w') as output_file:
+    output_file.write("[\n")
+
+    first_object = True
+
+    for path in paths:
+        with open(os.path.join(translated_json_path, path), 'r', encoding='utf-8-sig') as input_file:
+            for line in input_file:
+                if line.strip():
+                    if first_object:
+                        first_object = False
+                    else:
+                        output_file.write(',')
+                        output_file.write("\n")
+
+                    json_object = json.loads(line)  # Parsowanie linii do obiektu JSON
+                    json.dump(json_object, output_file, ensure_ascii=False, indent=4)  # Zapis obiektu JSON do pliku docelowego
+
+    output_file.write('\n]')  # Koniec tablicy JSON
+
+print("Combined file saved.")
